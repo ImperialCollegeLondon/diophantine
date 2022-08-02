d6a33a18f59e7c6be26072874a94ec9553caa9ad
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-08-02 15:35:48 +0100

    tidying up

--- src/Mordell/n.lean
+++ src/Mordell/n.lean
@@ -7,12 +7,15 @@ import number_theory.legendre_symbol.quadratic_reciprocity
 
 import Mordell.CongruencesMod4
 
+-- this should be PR'ed in Kevin's opinion!
+lemma int.modeq_def {a b n : ℤ} : a ≡ b [ZMOD n] ↔ a % n = b % n := iff.rfl
+
+-- as should this
+lemma nat.modeq_def {a b n : ℕ} : a ≡ b [MOD n] ↔ a % n = b % n := iff.rfl
+
 lemma int.exists_nat_of_nonneg {z : ℤ} (hz : 0 ≤ z) : ∃ n : ℕ, 
   (n : ℤ) = z := 
-begin
-  use z.nat_abs,
-  exact int.nat_abs_of_nonneg hz,
-end
+⟨z.nat_abs, int.nat_abs_of_nonneg hz⟩
 
 theorem modeq_add_fac_self {a t n : ℤ} : a + n * t ≡ a [ZMOD n] :=
 int.modeq_add_fac _ int.modeq.rfl
@@ -23,24 +26,23 @@ begin
   exact exists_congr (λ t, sub_eq_iff_eq_add'),
 end
 
-lemma prime_factor_congr_3_mod_4 (x a: ℤ) (h : x^2 - a*x + a^2 ≡ 3 [ZMOD 4]) :
-  ∃ (p : ℕ), p.prime ∧ (p : int) ∣ (x^2 - a*x + a^2)  ∧ p ≡ 3 [MOD 4] := 
+lemma prime_factor_congr_3_mod_4 (x a : ℤ) (h : x^2 - a*x + a^2 ≡ 3 [ZMOD 4]) :
+  ∃ (p : ℕ), p.prime ∧ (p : ℤ) ∣ (x^2 - a*x + a^2) ∧ p ≡ 3 [MOD 4] := 
 begin
-  have h1 : ((x^2 - a*x + a^2): ℚ) = (x-a/2)^2 + (3*a^2)/4:= by ring,
-  have h2 : 0 ≤ x^2 - a*x + a^2,
-  { nlinarith, },
+  have h2 : 0 ≤ x^2 - a*x + a^2 := by nlinarith,
   obtain ⟨n, hn⟩  := int.exists_nat_of_nonneg h2,
   rw ← hn at h,
   have h3 : n ≡ 3 [MOD 4],
-  unfold int.modeq at h,
-  unfold nat.modeq,
-  assumption_mod_cast,
+  { rw int.modeq_def at h,
+    rw nat.modeq_def,
+    assumption_mod_cast },
   obtain ⟨p, hp1, hp2, hp3⟩ := three_modulo_four_prime_factor n h3,
   refine ⟨p, hp1, _, hp3⟩,
   rwa [← hn, int.coe_nat_dvd],
 end
 
-lemma cube_minus_one_no_solns_aux (x y n a : ℤ) (hx : n ≡ 3 [ZMOD 4]) (ha : a ≡ 2 [ZMOD 4]) (hy : n + 1 = a^3): y^2 ≠ x^3 + n :=
+lemma cube_minus_one_no_solns_aux (x y n a : ℤ) (hx : n ≡ 3 [ZMOD 4]) 
+  (ha : a ≡ 2 [ZMOD 4]) (hy : n + 1 = a^3): y^2 ≠ x^3 + n :=
 begin
   intro heq,
   have oddx: odd x,
